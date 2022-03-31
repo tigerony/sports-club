285259fff64180783030c9368178b35b24e2b942
tigerony <williamlei95906@gmail.com>
2022-03-31 10:21:38 +0600

    add depandancy Bootstrap

--- src/Pages/Home/OurTeam/OurTeam.js
+++ src/Pages/Home/OurTeam/OurTeam.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OurTeam = () => {
          return (
-                  <div>
+                  <div >
                            
                   </div>
          );
