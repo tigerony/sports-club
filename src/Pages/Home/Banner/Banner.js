c1f172e7c962b395b2ee38aa18eda9785a4cea59
9abdurrahman2020 <9abdurrahman2020@gmail.com>
2022-03-31 17:32:21 +0600

    created new branch

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-    return (
-        <div>
-            <h2>this is Banner</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Abdur Rahman working with banner section !</h2>
+    </div>
+  );
 };
 
 export default Banner;
