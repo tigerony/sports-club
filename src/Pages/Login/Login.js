58b82f3e30b3f2fdb954672547608cbadb595d4c
nayeem407 <sabbirahmednayeem941@gmail.com>
2022-04-04 15:19:26 +0600

    login and register page desing

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
+
 import './Login.css'
 const Login = () => {
     return (
@@ -12,11 +14,30 @@ const Login = () => {
                 <span className='from-span'></span>
 
                 <div className='login-inner'>
-                    <h2 className='login-text'>LOGIN</h2>
+                    <h2 className='login-text'> <div className='about'>
+                                <div className='autoType'>
+                                    <Typewriter
+
+                                        options={{
+                                            strings: [
+                                                "Please Login",
+                                               
+                                            ],
+
+                                            autoStart: true,
+                                            loop: true,
+                                            delay: 100,
+                                        }}
+                                    />
+                                </div>
+                               
+                            </div></h2>
                     <div className='content-style'>
                         <input type="text" name="" id=""  placeholder='User name' className='user-text' />
                         <input type="password" name="" id=""  placeholder='password' className='user-text' />
-                        <Link as={Link} to='/register' >Please Register</Link>
+                        <Link as={Link} to='/register' style={{
+                            textDecration:'none',color:'#d7a3d7', marginTop:'20px'
+                        }} >Please Register</Link>
                         <button className='submit-btn'>Submit</button>
                     </div>
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Register.css'
+import Typewriter from 'typewriter-effect';
+
 
 const Register = () => {
     return (
@@ -12,10 +14,29 @@ const Register = () => {
                 <span className='from-span'></span>
 
                 <div className='login-inner'>
-                    <h2 className='login-text'>REGISTER</h2>
+                    <h2 className='login-text'><div className='about'>
+                                <div className='autoType'>
+                                    <Typewriter
+
+                                        options={{
+                                            strings: [
+                                                "Please Register",
+                                               
+                                            ],
+
+                                            autoStart: true,
+                                            loop: true,
+                                            delay: 100,
+                                        }}
+                                    />
+                                </div>
+                               
+                            </div></h2>
                     <div className='content-style'>
                         <input type="text" name="" id=""  placeholder='User name' className='user-text' />
+                        <input type="text" name="" id=""  placeholder='Email' className='user-text' />
                         <input type="password" name="" id=""  placeholder='password' className='user-text' />
+                        <input type="password" name="" id=""  placeholder='Re-password' className='user-text' />
                         <button className='submit-btn'>Submit</button>
                     </div>
 
