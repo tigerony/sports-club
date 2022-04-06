02f1988293812cf3fd15062667dc84d23cd58f14
9abdurrahman2020 <9abdurrahman2020@gmail.com>
2022-04-06 21:34:56 +0600

    eventRegister component work done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Booking from "./Pages/Booking/Booking";
+import EventRegister from "./Pages/EventRegister/EventRegister";
+import FootBallHome from "./Pages/Football/FootBallHome/FootBallHome";
 import Home from "./Pages/Home/Home/Home";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Navigation from "./Pages/Shared/Navigation/Navigation";
-
-
-import FootBallHome from "./Pages/Football/FootBallHome/FootBallHome";
-
 
 function App() {
   return (
@@ -25,6 +23,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/booking" element={<Booking />} />
+          <Route path="/event-register" element={<EventRegister />} />
         </Routes>
         <Footer />
       </Router>
